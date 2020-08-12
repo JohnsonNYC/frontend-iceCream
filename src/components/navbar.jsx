dbe4aa1c8fb38ce7f8d0ca1564e08860ad5292f5
JohnsonNYC <jkow95@gmail.com>
2020-08-12 15:35:44 -0400

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                Carlos is a dick
+            </div>
+        );
+    }
+}
+
+export default Navbar;
